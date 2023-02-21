9e87e1c0d63570e77b485d414d81667e97bdeca7
Amen TETTEH <tkamen@outlook.com>
2023-02-21 19:07:57 +0000

    Update game.rb file to add can_be_archived method

--- entities/game.rb
+++ entities/game.rb
@@ -1,5 +1,7 @@
 # class Game
 require_relative '../item'
+require 'date'
+
 class Game < Item
   attr_reader :id
   attr_accessor :multiplayer, :last_played_at
@@ -10,4 +12,9 @@ class Game < Item
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
+
+  def can_be_archived?()
+    super && (DateTime.now - Date.parse(last_played_at)).to_i / 365 > 2
+  end
+
 end
