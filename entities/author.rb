7efe253c999f70170a5edbd0604f192d6829535f
Amen TETTEH <tkamen@outlook.com>
2023-02-21 18:51:13 +0000

    Create  author.rb for Author class and add properties

---
+++ entities/author.rb
@@ -0,0 +1,12 @@
+# class Author
+class Author
+  attr_reader :id
+  attr_accessor :first_name, :last_name, :items
+
+  def initialize(first_name, last_name)
+    @id = Random.rand(1...1000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+end
