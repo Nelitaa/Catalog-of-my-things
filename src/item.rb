6b7a8e694368cec229c6fd91107f1cacc3bb1369
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-02-20 22:14:27 -0400

    Add custom setter methods

--- src/item.rb
+++ src/item.rb
@@ -4,4 +4,20 @@ class Item
     @publish_date = publish_date
     @archived = archived
   end
+
+  def genre(genre)
+    @genre = genre
+  end
+
+  def author(author)
+    @author = author
+  end
+
+  def source(source)
+    @source = source
+  end
+
+  def label(label)
+    @label = label
+  end
 end
