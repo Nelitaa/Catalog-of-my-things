dbb6f53969f3453ad117acbc8d6e4f572a6c8319
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-02-22 16:21:28 -0400

    Create table item

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -1,3 +1,15 @@
+-- Create Item Table
+CREATE TABLE item (
+  id              integer GENERATED ALWAYS AS IDENTITY,
+  genre           varchar(100),
+  author          varchar(100),
+  source          varchar(100),
+  label           varchar(100),
+  publish_date    date,
+  archived        boolean
+  PRIMARY KEY(id)
+);
+
 -- Create Games Table
 CREATE TABLE games (
   id INT,
