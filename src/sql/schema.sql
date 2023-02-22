d56855bd81b57371578769824748d1a7f03d480e
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-02-22 16:28:46 -0400

    Add music_album and genre create table

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -25,3 +25,19 @@ CREATE TABLE authors (
   last_name VARCHAR(255),
   PRIMARY KEY(id)
 );
+
+-- Create MusicAlbum Table
+CREATE TABLE music_album (
+  id              integer GENERATED ALWAYS AS IDENTITY,
+  publish_date    date,
+  on_spotify      boolean,
+  genre_id        int references genre(id),
+  PRIMARY KEY(id)
+);
+
+-- Create Genre Table
+CREATE TABLE genre(
+  id              integer GENERATED ALWAYS AS IDENTITY,
+  name            varchar(100),
+  PRIMARY KEY(id)
+);
