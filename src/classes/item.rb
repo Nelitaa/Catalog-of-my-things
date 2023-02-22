56a03fa065342db5a70ecdd32cd19069373c4340
Amen TETTEH <tkamen@outlook.com>
2023-02-22 16:05:47 +0000

    Update item.rb

--- src/classes/item.rb
+++ src/classes/item.rb
@@ -4,7 +4,7 @@ class Item
   attr_reader :id
   attr_accessor :publish_date, :archived, :genre, :author, :source, :label
 
-  def initialize(_id, publish_date, archived: false)
+  def initialize(publish_date, archived: false)
     @id = Random.rand(1...1000)
     @publish_date = Date.parse(publish_date)
     @archived = archived
@@ -29,7 +29,7 @@ class Item
 
   def can_be_archived?
     current_date = Date.today
-    year = current_date.year - @publish_date.year
+    year = current_date - @publish_date.year
     year >= 10
   end
 
