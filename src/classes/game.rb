bd52aaa10060cb55169cdda847e6c21e3a75eebb
Amen TETTEH <tkamen@outlook.com>
2023-02-22 15:12:17 +0000

    Update author.rb and game.rb files

--- src/classes/game.rb
+++ src/classes/game.rb
@@ -1,12 +1,13 @@
 # class Game
-require_relative '../item'
+require_relative './item'
 require 'date'
+require 'json'
 
 class Game < Item
   attr_reader :id
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(publish_date, last_played_at, multiplayer: false)
+  def initialize(publish_date, last_played_at, multiplayer)
     super(publish_date)
     @id = Random.rand(1...1000)
     @multiplayer = multiplayer
@@ -15,5 +16,18 @@ class Game < Item
 
   def can_be_archived?()
     super && (DateTime.now - Date.parse(last_played_at)).to_i / 365 > 2
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'publish_date' => @publish_date,
+      'multiplayer' => @multiplayer,
+      'last_played_at' => @last_played_at
+    }.to_json(*args)
+  end
+
+  def self.json_create(object)
+    new(object['publish_date'], object['multiplayer'], object['last_played_at'])
   end
 end
